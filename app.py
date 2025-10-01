cdd5e749f77e9eff243f7346aec2d2c218c1eed8
imbcoffee <imakebettercoffee@gmail.com>
2025-10-01 17:02:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+from flask import Flask, render_template, redirect, url_for, send_from_directory
+
+app = Flask(__name__)
+MOVIE_FOLDER = "E:/Watch"
+
+@app.route('/')
+def index():
+    movies = [f for f in os.listdir(MOVIE_FOLDER) if f.lower().endswith(('.mp4', '.mkv', '.avi', '.mov'))]
+    return render_template('index.html', movies=movies)
+
+@app.route('/play/<path:filename>')
+def play(filename):
+    path = os.path.abspath(os.path.join(MOVIE_FOLDER, filename))
+    if os.path.exists(path):
+        subprocess.Popen([r"C:\Program Files\VideoLAN\VLC\vlc.exe", path])
+    return redirect(url_for('index'))
+
+@app.route('/movies/<path:filename>')
+def serve_movie(filename):
+    return send_from_directory(MOVIE_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run()
